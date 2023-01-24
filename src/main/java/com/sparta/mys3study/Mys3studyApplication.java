2c158773c5e6903b38683377269a252aa3d9afb1
JxxHxxx <leesin5498@naver.com>
2023-01-24 20:03:21 +0900

    github action - datasource 예외 테스트
    스프링 부트 버전 수정

--- src/main/java/com/sparta/mys3study/Mys3studyApplication.java
+++ src/main/java/com/sparta/mys3study/Mys3studyApplication.java
@@ -2,9 +2,8 @@ package com.sparta.mys3study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class Mys3studyApplication {
 
     public static void main(String[] args) {
