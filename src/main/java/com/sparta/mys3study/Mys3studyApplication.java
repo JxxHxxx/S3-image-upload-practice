ef862d02c966db8b8525315f0d77587cc7c7f6fe
JxxHxxx <leesin5498@naver.com>
2023-01-24 21:01:05 +0900

    github action - RDS - EC2 연결 후 시도

--- src/main/java/com/sparta/mys3study/Mys3studyApplication.java
+++ src/main/java/com/sparta/mys3study/Mys3studyApplication.java
@@ -3,6 +3,7 @@ package com.sparta.mys3study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//추가시도
 @SpringBootApplication
 public class Mys3studyApplication {
 
